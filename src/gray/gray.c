d4101effb664233181d5eb666245b4dbf87d36f3
Oliver Caldwell <olliec87@gmail.com>
2013-09-03 20:15:43 +0100

    Simplified gray methods.
    Now using unsigned ints instead of chars too.

--- src/gray/gray.c
+++ src/gray/gray.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-char binary_to_gray(char *c)
+unsigned int binary_to_gray(unsigned int c)
 {
-	return (*c >> 1) ^ *c;
+	return (c >> 1) ^ c;
 }
 
-char gray_to_binary(char *c)
+unsigned int gray_to_binary(unsigned int c)
 {
 	unsigned int mask;
-	char res = *c;
 
-	for (mask = *c >> 1; mask != 0; mask = mask >> 1) {
-		res = res ^ mask;
+	for (mask = c >> 1; mask != 0; mask >>= 1) {
+		c ^= mask;
 	}
 
-	return res;
+	return c;
 }
 
 int main()
@@ -30,14 +29,14 @@ int main()
 	printf("Input contains %i bytes.\n", length);
 
 	for (i = 0; i < length; i++) {
-		output[i] = binary_to_gray(&input[i]);
+		output[i] = binary_to_gray(input[i]);
 	}
 
 	printf("Output is: \"%s\".\n", output);
 	printf("Now back the other way!\n");
 
 	for (i = 0; i < length; i++) {
-		reverse[i] = gray_to_binary(&output[i]);
+		reverse[i] = gray_to_binary(output[i]);
 	}
 
 	printf("Reversed output is: \"%s\".\n", reverse);
